ca1e5e3e7ea30f6cd829e3c7e0128bd7575c8a1d
6th Hour <bg127203@student.wpsok.org>
2026-09-11 13:56:08 -0500

    Submit HW5 AHAHAHAHAHA

---
+++ HW6.py
@@ -0,0 +1,41 @@
+#Name:Braylee Gramer
+
+#Class: 6th Hour
+
+#Assignment: HW6
+
+#1. Create a list with 9 different numbers inside.
+numlist=[26,4,6,8,10,12,14,16,18]
+
+#2. Sort the list from highest to lowest.
+numlist.sort()
+print(numlist)
+
+#3. Create an empty list.
+nolist=[]
+
+#4. Remove the median number from the first list and add it to the second list.
+numlist.remove(4)
+print(numlist)
+nolist.append(4)
+
+#5. Remove the first number from the first list and add it to the second list.
+numlist.remove(26)
+print(numlist)
+nolist.append(26)
+
+#6. Print both lists.
+print(nolist)
+print(numlist)
+
+#7. Add the two numbers in the second list together and print the result.
+nolistsubsum=nolist[0]+nolist[1]
+print(nolistsubsum)
+
+#8. Add the sum from #7 to the first list.
+numlist.append(nolistsubsum)
+print(numlist)
+
+#9. Sort the first list from lowest to highest and print it.
+numlist.sort()
+print(numlist)
